36568b8c8088c692572d3dcc1954ce88b71cba9a
Stephen Arancio <sjarancio@gmail.com>
2021-07-06 17:28:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "1969", result: "W"},
+    {year: "2016", result: "N/A"}
+  ];
 
+// function blueFilter (collection) {
+//     for (const user of collection) {
+//       if (user.favoriteColor === 'Blue') {
+//         console.log(user.firstName);
+//       }
+//     }
+//   }
+
+ function superbowlWin(collection){
+    for (const user of collection) {
+        if (user.result === "W") {
+            return user.year
+        }
+    }
+ }
+
+ superbowlWin(record);
+
+//  superbowlWin(record);
